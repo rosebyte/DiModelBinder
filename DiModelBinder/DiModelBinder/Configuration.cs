0d055ffc7e0f5eaaa77c1f6254b54bc44b43a838
jaroslav <jaroslav.ruzicka@hotmail.com>
2019-03-10 00:00:07 +0100

    add Configuration to enable model binding DI in MVC

--- DiModelBinder/DiModelBinder/Configuration.cs
+++ DiModelBinder/DiModelBinder/Configuration.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiModelBinder
 {
-	class Configuration
+	public static class Configuration
 	{
+		public static void AddModelBinderProvider(this MvcOptions options) =>
+			options.ModelBinderProviders.Insert(0, new ModelBinderProvider());
+
+		public static void AddModelBindingDiResolver(this IServiceCollection services) => 
+			services.AddSingleton<DiDependenciesResolver>();
 	}
 }
