11e242543c85fb9c813a51a4bcf1d85ac25044d7
jaroslav <jaroslav.ruzicka@hotmail.com>
2019-03-09 23:56:05 +0100

    add ResolveWithAttribute

---
+++ DiModelBinder/DiModelBinder/ResolveWithAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+	[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Class)]
+	public class ResolveWithAttribute : Attribute
+	{
+		public Type Type;
+
+		public ResolveWithAttribute(Type type)
+		{
+			Type = type;
+		}
+	}
+}
