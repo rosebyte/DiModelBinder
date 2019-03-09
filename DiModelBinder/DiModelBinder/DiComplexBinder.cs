5df2c422c6970a139610aecb80d2e069f8f365a2
jaroslav <jaroslav.ruzicka@hotmail.com>
2019-03-09 23:59:22 +0100

    change binder to use resolver

--- DiModelBinder/DiModelBinder/DiComplexBinder.cs
+++ DiModelBinder/DiModelBinder/DiComplexBinder.cs
@@ -9,10 +9,21 @@ namespace DiModelBinder
 {
 	public class DiComplexBinder : ComplexTypeModelBinder
 	{
-		public DiComplexBinder(BindingDictionary propertyBinder, ILoggerFactory logger)
-			: base(propertyBinder, logger) { }
+		private readonly DiDependenciesResolver _resolver;
 
-		protected override object CreateModel(ModelBindingContext bindingContext) => 
-			bindingContext.HttpContext.RequestServices.GetService(bindingContext.ModelType);
+		public DiComplexBinder(
+			BindingDictionary propertyBinder,
+			ILoggerFactory logger,
+			DiDependenciesResolver resolver) : base(propertyBinder, logger)
+		{
+			_resolver = resolver;
+		}
+
+		protected override object CreateModel(ModelBindingContext bindingContext)
+		{
+			return _resolver != null
+				? _resolver.ResolveModel(bindingContext.ModelType, bindingContext.HttpContext.RequestServices)
+				: bindingContext.HttpContext.RequestServices.GetService(bindingContext.ModelType);
+		}
 	}
 }
